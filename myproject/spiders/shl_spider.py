6c00b047414c1560b52673d69e43c1514807d20e
EIK <emilingemarkarlsson@gmail.com>
2024-07-02 10:32:05 +0200

    Your commit message

--- myproject/spiders/shl_spider.py
+++ myproject/spiders/shl_spider.py
@@ -1,15 +1,21 @@
 import scrapy
 from myproject.items import MyprojectItem
+from apify_client import ApifyClient
 
 class ShlSpider(scrapy.Spider):
     name = "shl"
     start_urls = ['https://old.shl.se/statistik/tabell?season=2023&gameType=regular']
 
     def parse(self, response):
-        rows = response.xpath('//table/tbody/tr')
+        client = ApifyClient()
+        dataset = client.datasets().get_or_create(name='shl-dataset')
+
+        rows = response.xpath('//tr')
         for row in rows:
             item = MyprojectItem()
             item['position'] = row.xpath('./td[1]/text()').get()
             item['team'] = row.xpath('./td[2]/a/span[1]/text()').get()
             item['games_played'] = row.xpath('./td[3]/text()').get()
+            dataset.items().put(item)
+
             yield item
