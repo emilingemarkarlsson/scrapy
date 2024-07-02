32c74cf1a27708da962a35270ec68773ad5fa144
EIK <emilingemarkarlsson@gmail.com>
2024-07-02 10:51:50 +0200

    Your commit message

--- myproject/spiders/shl_spider.py
+++ myproject/spiders/shl_spider.py
@@ -6,11 +6,16 @@ class ShlSpider(scrapy.Spider):
     name = "shl"
     start_urls = ['https://old.shl.se/statistik/tabell?season=2023&gameType=regular']
 
+    def __init__(self, *args, **kwargs):
+        super(ShlSpider, self).__init__(*args, **kwargs)
+        self.client = ApifyClient()
+        # Create or get the default dataset associated with this actor run
+        self.dataset = self.client.datasets().get_or_create(name='default')
+    
     def parse(self, response):
-        client = ApifyClient()
-        dataset = client.datasets().get_or_create(name='shl-dataset')
-
         rows = response.xpath('//tr')
+        items_to_store = []
+        
         for row in rows:
             item = MyprojectItem()
             item['position'] = row.xpath('td[1]/text()').get()
@@ -24,7 +29,10 @@ class ShlSpider(scrapy.Spider):
             item['goals_against'] = row.xpath('td[9]/text()').get()
             item['goal_difference'] = row.xpath('td[10]/text()').get()
             item['points'] = row.xpath('td[11]/text()').get()
+            items_to_store.append(item)
+        
+        # Store items in the Apify dataset
+        self.dataset.items().put_multiple(items_to_store)
 
-            dataset.items().put(item)
-
+        for item in items_to_store:
             yield item
